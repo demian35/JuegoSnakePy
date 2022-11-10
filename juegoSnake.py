ecf3f2429710ca74cbce209fc9688d2f01ac3ae0
Demian <53890358+demian35@users.noreply.github.com>
2022-11-10 01:47:26 -0600

    declaramos los fps del juego y la ventana interfaz del juego

---
+++ juegoSnake.py
@@ -0,0 +1,14 @@
+import pygame , sys,time,random #bibliotecas necesarias para el desarrollo
+
+#inicializamos pygame
+pygame.init()
+
+
+#interfaz proporcionada por la biblioteca pygame le pasamos las medidas del alto y ancho
+interfaz_game=pygame.display.set_mode(500,500)
+
+#fps del juego
+fps=pygame.time.Clock()
+
+def main():
+    pass
